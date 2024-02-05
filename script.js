3db18906e77dee3bebc2828dd16fd14d1065be03
unknown <Administrador@SU240-097125-L.CampusUninorte.local>
2024-02-05 18:24:19 -0500

    lab 1 finish

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const cantidad = 5
+
+const prime = primo => {
+
+    let divisores = 0;
+
+    for(let i = 1; i<=primo; i++){
+        if (primo % i == 0){
+            divisores++;
+        }
+    }
+    if(divisores == 2){
+        return true
+    }
+
+    return false
+}
+
+const cantidad = 15
 
 let i = 0
 let primoTemp = 1
 
-while(i<cantidad){
-
+while (i < cantidad) {
+    if(prime(primoTemp)){
+        console.log(i+1, "->", primoTemp)
+        i++
+    }
+    primoTemp++
 }
